065693cb45c3b0641c525825ea7f47c2c6bb748d
Ariel Beroíza <beroiza.ariel@gmail.com>
2026-03-17 04:17:13 -0300

    fix: minimize logo area padding

--- frontend/components/SideNav.tsx
+++ frontend/components/SideNav.tsx
@@ -319,7 +319,7 @@ export default function SideNav() {
       <div
         className="flex items-center justify-center shrink-0"
         style={{
-          padding: "12px 20px 10px",
+          padding: "4px 20px 2px",
           borderBottom: "1px solid rgba(255,255,255,0.1)"
         }}
       >
